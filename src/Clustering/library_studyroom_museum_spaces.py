3434b92aac9d93ef0d4b9e160222ab0104c3ef68
pierreachkar1717 <Dj\piero@1717!>
2022-12-27 19:07:59 +0100

    now it'S ready

--- src/Clustering/library_studyroom_museum_spaces.py
+++ src/Clustering/library_studyroom_museum_spaces.py
@@ -1,11 +1,14 @@
 from clustering import optimal_clusters_number, cluster_and_plot
 import pandas as pd
 
+
+## Each step should be done separately, so comment the other functions out when running one of them.
+
 # Load your data into a pandas DataFrame
-df = pd.read_csv('../../data/Processed/neighbourhood/library_studyroom_museum_spaces_neighbourhood.csv')
+#df = pd.read_csv('../../data/Processed/neighbourhood/library_studyroom_museum_spaces_neighbourhood.csv')
 
 # Choose the column/columns in your data that you want to use for clustering
-cols = ['library_studyroom_museum_spaces']
+#cols = ['library_studyroom_museum_spaces']
 
 # First: Call the optimal_clusters_number function to determine the optimal number of clusters for your data
 # Decide on the best number of clusters for your data by considering both plots
@@ -17,4 +20,16 @@ cols = ['library_studyroom_museum_spaces']
 #optimal_clusters_number(df, cols, 20)
 
 # Second: Call the cluster_and_plot function to cluster your data and plot the results
-cluster_and_plot(df, cols, 2, 'library_studyroom_museum_spaces')
+#cluster_and_plot(df, cols, 3, 'library_studyroom_museum_spaces')
+
+# Third: Load the resulting csv file into a new pandas DataFrame and add a new column to the DataFrame
+# This column will contain numerical values describing the cluster label for each data point.
+#df = pd.read_csv('../../data/Processed/clustering/clustered_library_studyroom_museum_spaces.csv')
+
+# Here 0 means low, 1 means medium and 2 means high
+#df['label'] = df['cluster'].map({0: 1, 1: 2, 2: 0})
+
+# Or we can still consider adding categorical values to the label column
+#df['label_cat'] = df['cluster'].map({0: 'medium_number_library_studyroom', 1: 'high_number_library_studyroom', 2: 'low_number_library_studyroom'})
+
+#df.to_csv('../../data/Processed/clustering/labled/labled_library_studyroom_museum_spaces.csv', index=False)
