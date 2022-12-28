b772cf434b516e7cac6affebed4d2d39822d4862
odelachapelle <olivia33@free.fr>
2022-12-28 11:37:09 +0100

    between_18_64 done

--- src/Clustering/between_18_64.py
+++ src/Clustering/between_18_64.py
@@ -5,10 +5,10 @@ import pandas as pd
 ## Each step should be done separately, so comment the other functions out when running one of them.
 
 # Load your data into a pandas DataFrame
-#df = pd.read_csv('../../data/Processed/neighbourhood/library_studyroom_museum_spaces_neighbourhood.csv')
+#df = pd.read_csv('../../data/Processed/neighbourhood/2022_between_18_64_neighbourhood.csv')
 
 # Choose the column/columns in your data that you want to use for clustering
-#cols = ['library_studyroom_museum_spaces']
+#cols = ['between_18_64']
 
 # First: Call the optimal_clusters_number function to determine the optimal number of clusters for your data
 # Decide on the best number of clusters for your data by considering both plots
@@ -20,16 +20,16 @@ import pandas as pd
 #optimal_clusters_number(df, cols, 20)
 
 # Second: Call the cluster_and_plot function to cluster your data and plot the results
-#cluster_and_plot(df, cols, 3, 'library_studyroom_museum_spaces')
+#cluster_and_plot(df, cols, 4, 'between_18_64')
 
 # Third: Load the resulting csv file into a new pandas DataFrame and add a new column to the DataFrame
 # This column will contain numerical values describing the cluster label for each data point.
-#df = pd.read_csv('../../data/Processed/clustering/clustered_library_studyroom_museum_spaces.csv')
+df = pd.read_csv('../../data/Processed/clustering/clustered_between_18_64.csv')
 
 # Here 0 means low, 1 means medium and 2 means high
-#df['label'] = df['cluster'].map({0: 1, 1: 2, 2: 0})
+df['label'] = df['cluster'].map({0: 1, 1: 2, 2: 0})
 
 # Or we can still consider adding categorical values to the label column
 #df['label_cat'] = df['cluster'].map({0: 'medium_number_library_studyroom', 1: 'high_number_library_studyroom', 2: 'low_number_library_studyroom'})
 
-#df.to_csv('../../data/Processed/clustering/labled/labled_library_studyroom_museum_spaces.csv', index=False)
+df.to_csv('../../data/Processed/clustering/labled/labled_between_18_64.csv', index=False)
